b469dfe40c9043da0e21ac2fbfc896b18e3d8f9f
emoon <emoon@qq.com>
2021-05-28 11:23:50 +0800

    weather place search

---
+++ app/src/main/java/com/tujsok/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.tujsok.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+        const val TOKEN = "4jJmezgUvYugNWCG"
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/tujsok/sunnyweather/ui/place/PlaceViewModel.kt
@@ -0,0 +1,22 @@
+package com.tujsok.sunnyweather.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.tujsok.sunnyweather.logic.Repository
+import com.tujsok.sunnyweather.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+}
